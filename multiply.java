05da6aafa28864135e8f08eb8b3a67db489fe0a1
Hara-97 <240301120382@centurionuniv.edu.in>
2025-07-11 07:34:21 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+    }
+}
